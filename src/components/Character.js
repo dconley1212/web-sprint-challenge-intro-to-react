06dc6c1557457ff428125fbcf6093df6498c94c1
David Conley <dconley1989@gmail.com>
2021-12-10 09:46:20 -0700

    rendered all the character names to the screen

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from "react";
+
+export default function Character(props) {
+  const { character } = props;
+  console.log(character);
+
+  return (
+    <div>
+      <h2>{character.name}</h2>
+    </div>
+  );
+}
